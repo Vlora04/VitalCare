99c1ddc4b24448f085ce5c167d55bdfe1b2f90e2
vskeraj <vs71529@ubt-uni.net>
2025-05-09 10:59:13 +0200

    perkthimi

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -5,15 +5,15 @@ import { useState } from 'react';
 
 const MyProfile = () => {
   const [userData, setUserData] = useState({
-    name: "Edward Vincent",
+    name: "Alban Shaqiri",
     image: assets.profile_pic,
-    email: 'richardjameswap@gmail.com',
-    phone: '+1 233 456 7890',
+    email: 'albanshaqiri@gmail.com',
+    phone: '+383 44 123 456',
     address: {
-      line1: "57th Cross, Richmond ",
-      line2: "Circle, Church Road, London"
+      line1: "12th Street, Bregu i Diellit ",
+      line2: "Arbëria District, Prishtina"
     },
-    gender: 'Male',
+    gender: 'Mashkull',
     dob: '2000-01-20'
   })
 
@@ -31,17 +31,17 @@ const MyProfile = () => {
 
       <hr className='bg-zinc-400 h-[1px] border-none' />
       <div>
-        <p className='text-neutral-500 underline mt-3'>CONTACT INFORMATION</p>
+        <p className='text-neutral-500 underline mt-3'>INFORMATAT E KONTAKTIMIT</p>
         <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
           <p className='font-medium'>Email id:</p>
           <p className='text-blue-500'>{userData.email}</p>
-          <p className='font-medium'>Phone:</p>
+          <p className='font-medium'>Numri i telefonit:</p>
           {
             isEdit
               ? <input className='bg-gray-100 max-w-52' type="text" value={userData.phone} onChange={e => setUserData(prev => ({ ...prev, phone: e.target.value }))} />
               : <p className='text-blue-400'>{userData.phone}</p>
           }
-          <p className='font-medium'>Address:</p>
+          <p className='font-medium'>Adresa:</p>
           {
             isEdit
               ? <p>
@@ -58,18 +58,18 @@ const MyProfile = () => {
         </div>
       </div>
       <div>
-        <p className='text-neutral-500 underline mt-3'>BASIC INFORMATION</p>
+        <p className='text-neutral-500 underline mt-3'>INFORMATAT E PËRGJITHSHME</p>
         <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
-          <p className='font-medium'>Gender:</p>
+          <p className='font-medium'>Gjinia:</p>
           {
             isEdit
               ? <select className='max-w-20 bg-gray-100' onChange={(e) => setUserData(prev => ({ ...prev, gender: e.target.value }))} value={userData.gender}>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
+                <option value="Mashkull">Mashkull</option>
+                <option value="Femër">Femër</option>
               </select>
               : <p className='text-gray-400'>{userData.gender}</p>
           }
-          <p className='font-medium'>Birthday:</p>
+          <p className='font-medium'>Datëlindja:</p>
           {
             isEdit
               ? <input className='max-w-28 bg-gray-100' type="date" onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} value={userData.dob} />
@@ -81,7 +81,7 @@ const MyProfile = () => {
       <div className='mt-10'>
         {
           isEdit
-            ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={() => setIsEdit(false)}>Save information</button>
+            ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={() => setIsEdit(false)}>Ruaj informatat</button>
             : <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={() => setIsEdit(true)}>Edit</button>
         }
       </div>
